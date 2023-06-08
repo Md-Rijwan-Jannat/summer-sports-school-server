495e730d6c0895d4ce0a951d7e9a83ebf741bc18
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-06-08 16:58:17 +0600

    server set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('summer school is running')
+});
+
+
+app.listen(port, () => {
+    console.log(`summer school port is ${port}`);
+})
